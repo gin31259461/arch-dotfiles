9a72c30552eee4c1aa804d4ffb223dddff5209b4
abner <qw0207060413@gmail.com>
2026-06-15 17:10:44 +0800

    refactor(hypr): create new repo (hypr)

--- .config/hypr/lua/hyprconf/animations.lua
+++
@@ -1,9 +0,0 @@
-local profile = require("hyprconf.profile")
-
-local M = {}
-
-function M.setup()
-  profile.load("animation")
-end
-
-return M
--- .config/hypr/profiles/animation/disabled.lua
+++
@@ -1,7 +0,0 @@
--- profile: Animations Disabled
-
-local profile = require("hyprconf.profile")
-
-profile.apply_animation({
-  enabled = false,
-})
